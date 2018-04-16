d1fa1c2d17611eff5c0fa17e87d617ab01ff25a3
Roee Hershko <RoeeH1@verifone.com>
2018-04-16 18:34:12 +0300

    initial

---
+++ redis/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const redis = require("redis");
+const client = redis.createClient({
+    host: 'redis'
+});
+const uuidv4 = require('uuid/v4');
+
+// Testing set only
+app.get('/set', (req, res) => {
+    const key = uuidv4();
+
+    client.set(uuidv4(), JSON.stringify(req.headers));
+
+    res.send('New redis key - ' + key);
+});
+
+// Testing set and get on the same request
+let oldKey = null;
+app.get('/set_get', (req, res) => {
+    const key = uuidv4();
+
+    client.set(uuidv4(), JSON.stringify(req.headers));
+
+    if (oldKey) {
+        client.get(oldKey,() => {
+            res.send('Old redis key - ' + key);
+        });
+    }
+    else {
+        oldKey = key;
+        res.send('New redis key - ' + key);
+    }
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
